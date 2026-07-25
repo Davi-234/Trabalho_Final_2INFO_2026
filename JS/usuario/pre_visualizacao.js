f3decbc18c4c458bd22d1ff0ce32e08e51337879
Davi <davileandrocos@gmail.com>
2026-07-24 23:55:32 -0300

    Reorganizei os arquivos, adicionei uma tela modal para denúncias na página de global/projeto

--- JS/usuario/pre_visualizacao.js
+++ JS/usuario/pre_visualizacao.js
@@ -1,11 +1,22 @@
 function alterar_PreVisualizacao() {
     let titulo = $('#nome').val();
     let descricao = $('#descricao').val();
+    let nivel = $('#nivel option:selected').text();
+    let tipo = $('#tipo option:selected').text();
 
+    if (titulo.trim() == '') {
+        titulo = 'Nome do Projeto';
+    }
     $('#preview_title').html(titulo);
+    if (descricao.trim() == '') {
+        descricao = "Aqui aparecerá a descrição do projeto informada no formulário, permitindo uma visão geral antes da publicação.";
+    } 
     $('#preview_description').html(descricao);
+
+    $('#preview_nivel').html(nivel);
+    $('#preview_type').html(tipo);
 }
 
-setInterval(function() {
-    alterar_PreVisualizacao()
-}, 100);
+$(':input').on('input', () => {
+    alterar_PreVisualizacao();
+});
